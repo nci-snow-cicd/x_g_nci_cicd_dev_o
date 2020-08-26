1fc32a8bc2d84871599b421a06c9a93c2a1cfb0b
Nadim Rizk <nadim.rizk@nih.gov>
2020-08-26 09:53:14 -0400

    Changes by 'Nadim Rizk' on 'CICD_Dev_Ops - 1.0.0' -- Build #1

---
+++ sn/CICD_Dev_Ops/Dictionary Entry/x_g_nci_cicd_dev_o_cicd_dev_ops_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : CICD_Dev_Ops
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-04-13 21:20:59
+ * Created By  : rizknt@nih.gov
+ * Updated On  : 2020-04-16 22:56:42
+ * Updated By  : rizknt@nih.gov
+ * URL         : /sys_dictionary.do?sys_id=f0c2a6d71b4cd010fe45ed39bc4bcb1d
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
