37ecf8d60782b46cccb7180bbe1871654436e2bb
Nadim Rizk <nadim.rizk@nih.gov>
2020-10-14 11:37:09 -0400

    Changes by 'Nadim Rizk' on 'CICD_Dev_Ops - 1.0.1' -- Build #1

---
+++ sn/CICD_Dev_Ops/Script Include/active_true/x_g_nci_cicd_dev_o.Test_Script_Include_Demo.js
@@ -0,0 +1,28 @@
+/* 
+ * Application : CICD_Dev_Ops
+ * ClassName   : sys_script_include
+ * Created On  : 2020-10-14 15:30:18
+ * Created By  : rizknt@nih.gov
+ * Updated On  : 2020-10-14 15:30:27
+ * Updated By  : rizknt@nih.gov
+ * URL         : /sys_script_include.do?sys_id=d7b67ce61bbb181094edddb6bc4bcbac
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author Nadim Rizk [rizknt@nih.gov]
+ * @memberof x_g_nci_cicd_dev_o.module:sys_script_include
+ */
+var Test_Script_Include_Demo = Class.create();
+Test_Script_Include_Demo.prototype = /** @lends x_g_nci_cicd_dev_o.module:sys_script_include.Test_Script_Include_Demo.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'Test_Script_Include_Demo'
+};
