27395db223457d62b96d27a6ca6a3fa306214f0d
Rizk <rizknt@nih.gov>
2021-03-31 12:19:09 -0400

    Changes by 'undefined' on 'CICD_Dev_Ops - 1.0.0' -- Build #1

--- sn/CICD_Dev_Ops/Dictionary Entry/x_g_nci_cicd_dev_o_cicd_dev_ops_table/active_true/collection/calculation.js
+++ sn/CICD_Dev_Ops/Dictionary Entry/x_g_nci_cicd_dev_o_cicd_dev_ops_table/active_true/collection/calculation.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_dictionary
  * Created On  : 2020-04-13 21:20:59
  * Created By  : rizknt@nih.gov
- * Updated On  : 2020-04-16 22:56:42
- * Updated By  : rizknt@nih.gov
+ * Updated On  : 2021-03-31 10:44:03
+ * Updated By  : system
  * URL         : /sys_dictionary.do?sys_id=f0c2a6d71b4cd010fe45ed39bc4bcb1d
  */
 (function calculatedFieldValue(current) {
